594964c940a518181158d89f486eece42b8221c4
ilyapiatykh <ilya05072002@gmail.com>
2025-03-06 17:49:47 +0300

    fixes

--- app/db.py
+++ app/db.py
@@ -11,52 +11,53 @@ class DB:
         self.cursor.execute(
             """
                 CREATE TABLE IF NOT EXISTS sent_messages (
-                    target_message_id INTEGER PRIMARY KEY,
-                    source_message_id INTEGER,
-                    source_channel_id INTEGER,
-                    source_message_hash INTEGER
+                    id INTEGER AUTO INCREMENT PRIMARY KEY,
+                    target_msg_id INTEGER,
+                    src_msg_id INTEGER,
+                    src_msg_hash TEXT,
+                    src_channel_id INTEGER
                 );
             """
         )
 
         self.conn.commit()
 
-    def message_exists(
-        self,
-        source_channel_id: int,
-        source_message_hash: int,
-    ) -> bool:
-        self.cursor.execute(
-            "SELECT * FROM sent_messages WHERE source_channel_id = ? AND source_message_hash = ?",
-            (source_channel_id, source_message_hash),
-        )
-        return self.cursor.fetchone() is not None
-
     def save(
         self,
-        source_message_id: int,
-        target_message_id: int,
-        source_channel_id: int,
-        source_message_hash: int,
+        target_msg_id: int,
+        src_msg_id: int,
+        src_msg_hash: str,
+        src_channel_id: int,
     ):
         self.cursor.execute(
-            "INSERT INTO sent_messages (source_message_id, target_message_id, source_channel_id, source_message_hash) VALUES (?, ?, ?, ?)",
+            "INSERT INTO sent_messages (target_msg_id, src_msg_id, src_msg_hash, src_channel_id) VALUES (?, ?, ?, ?)",
             (
-                source_message_id,
-                target_message_id,
-                source_channel_id,
-                source_message_hash,
+                target_msg_id,
+                src_msg_id,
+                src_msg_hash,
+                src_channel_id,
             ),
         )
 
         self.conn.commit()
 
-    def get_target_message_id(
-        self, source_message_id: int, source_channel_id: int
+    def check_duplicate(
+        self, src_msg_hash: str, src_channel_id: int
     ) -> int | None:
         self.cursor.execute(
-            "SELECT target_message_id FROM sent_messages WHERE source_message_id = ? AND source_channel_id = ?",
-            (source_message_id, source_channel_id),
+            "SELECT target_msg_id FROM sent_messages WHERE src_msg_hash = ? AND src_channel_id = ?",
+            (src_msg_hash, src_channel_id),
+        )
+        result = self.cursor.fetchone()
+
+        return result[0] if result else None
+    
+    def get_target_msg_id(
+        self, src_msg_id: str, src_channel_id: int
+    ) -> int | None:
+        self.cursor.execute(
+            "SELECT target_msg_id FROM sent_messages WHERE src_msg_id = ? AND src_channel_id = ?",
+            (src_msg_id, src_channel_id),
         )
         result = self.cursor.fetchone()
 
--- app/main.py
+++ app/main.py
@@ -1,26 +1,23 @@
-import logging
 import re
+from hashlib import sha256
 from typing import Any
 
+from loguru import logger
 from telethon import TelegramClient, events
 
 from app.config import Config
 from app.db import DB
 
 TG_SESSION_FILE_PATH = "data/tg_syncer"
-LOGFILE_PATH = "data/parser.log"
+LOGFILE_PATH = "data/tg_syncer.log"
 
 
-def remove_binance_line(text: str) -> str:
-    return (
-        "\n".join(
-            line
-            for line in text.split("\n")
-            if not re.search(r"🆔\s*@binance_360", line)
-        )
-        if text
-        else ""
-    )
+def format_text(text: str) -> str:
+    return re.sub(r"🆔\s*@binance_360", "", text)
+
+
+def hash_text(text: str) -> str:
+    return sha256(text.encode()).hexdigest()
 
 
 if __name__ == "__main__":
@@ -28,9 +25,7 @@ if __name__ == "__main__":
 
     db = DB()
 
-    logging.basicConfig(
-        filename="data/parser.log", level=logging.INFO, encoding="utf-8"
-    )
+    logger.add(LOGFILE_PATH)
 
     with TelegramClient(
         TG_SESSION_FILE_PATH,
@@ -39,63 +34,62 @@ if __name__ == "__main__":
     ) as client:
 
         @client.on(events.NewMessage(chats=config.source_channels_ids))
+        @logger.catch(
+            Exception,
+            message="failed to process new message",
+            reraise=False,
+            level="ERROR",
+        )
         async def handler(event: Any):
-            try:
-                message = event.message
-                source_channel_id = event.chat_id
-                logging.info(
-                    f"new message channel={source_channel_id} ID={message.id} text={message.text[:20]}..."
-                )
+            src_msg = event.message
+            src_channel_id = event.chat_id
 
-                if not message.text:
-                    logging.warning("message without text, skipping.")
+            with logger.contextualize(
+                channel=src_channel_id, msg_id=src_msg.id, text=src_msg.text
+            ):
+                logger.debug("new message")
+
+                if not src_msg.text:
+                    logger.debug("message without text, skip")
                     return
 
-                text = remove_binance_line(message.text)
-                text_hash = hash(text)
+                text = format_text(src_msg.text)
+                msg_hash = hash_text(text)
 
-                if db.message_exists(source_channel_id, text_hash):
-                    logging.warning("message was already sent, skipping.")
+                target_msg_id = db.check_duplicate(msg_hash, src_channel_id)
+
+                if target_msg_id:
+                    db.save(
+                        target_msg_id, src_msg.id, msg_hash, src_channel_id
+                    )
+                    logger.debug("save message duplicate and skip")
                     return
 
                 target_channel = await client.get_input_entity(
                     config.target_channel_id
                 )
 
-                if not message.reply_to_msg_id:
-                    sent_message = await client.send_message(
-                        target_channel, text
+                target_replied_msg_id = None
+                if src_msg.reply_to_msg_id:
+                    target_replied_msg_id = db.get_target_msg_id(
+                        src_msg.reply_to_msg_id, src_channel_id
                     )
-                    db.save(
-                        message.id,
-                        sent_message.id,
-                        source_channel_id,
-                        text_hash,
-                    )
-                    logging.info(f"new message was sent: {text[:20]}...")
-                else:
-                    replied_message_id = message.reply_to_msg_id
-                    target_replied_message_id = db.get_target_message_id(
-                        replied_message_id, source_channel_id
-                    )
-                    if not target_replied_message_id:
-                        logging.warning(
-                            f"no matching message found for reply. ID: {replied_message_id}"
-                        )
+                    if not target_replied_msg_id:
+                        logger.warning("no matching message found for reply")
                         return
 
-                    sent_message = await client.send_message(
-                        target_channel, text, reply_to=target_replied_message_id
-                    )
-                    db.save(
-                        message.id,
-                        sent_message.id,
-                        source_channel_id,
-                        text_hash,
-                    )
-                    logging.info(f"reply was sent: {text[:20]}...")
-            except Exception as e:
-                logging.exception(f"error processing message: {e}")
+                sent_message = await client.send_message(
+                    target_channel, text, reply_to=target_replied_msg_id
+                )
 
-        logging.info("script was started")
+                db.save(
+                    sent_message.id,
+                    src_msg.id,
+                    msg_hash,
+                    src_channel_id,
+                )
+
+                logger.info("new message was sent")
+
+        logger.info("script was started")
         client.loop.run_until_complete(client.run_until_disconnected())
